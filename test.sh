8f3a75f1bef0e5f288a2ffcd593078f9d93aaf1b
Chris West (Faux) <git@goeswhere.com>
2017-04-04 18:30:25 +0100

    inline

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ set -u
 
 F=$1
 
-xsltproc --output html/ --xinclude /usr/share/xml/docbook/stylesheet/docbook-xsl/htmlhelp/htmlhelp.xsl "$F"  2>/dev/null || exit 2
+xsltproc --output html/ --xinclude docbook-xsl/htmlhelp/htmlhelp.xsl "$F"  2>/dev/null || exit 2
 
 for i in {1..50}; do
-    if xsltproc --output html/ --xinclude /usr/share/xml/docbook/stylesheet/docbook-xsl/htmlhelp/htmlhelp.xsl $F 2>&1 | fgrep 'Request for title of element with no title:'; then
+    if xsltproc --output html/ --xinclude docbook-xsl/htmlhelp/htmlhelp.xsl $F 2>&1 | fgrep 'Request for title of element with no title:'; then
         echo "broke at step $i"
         exit 0
     fi
